340b3b777cfa387b1a197519f8b8cff70edf4d73
JaffetR <jaffetchanco10@gmail.com>
2024-05-04 13:32:29 -0500

    Final 2.0

--- script.js
+++ script.js
@@ -9,39 +9,52 @@ let isDeleting = false;
 let timer;
 
 function typeEffect() {
+  let text = githubText.substring(0, index);
+
   if (isDeleting) {
-    if (githubElement.textContent.length > 0) {
-      githubElement.textContent = githubElement.textContent.substring(0, githubElement.textContent.length - 1);
-    } else {
-      isDeleting = false;
-      index = 0;
-    }
+    text = text.substring(0, text.length - 1);
+    index--;
   } else {
-    if (index < githubText.length) {
-      githubElement.textContent += githubText[index];
-      index++;
-    } else {
-      isDeleting = true;
-    }
+    index++;
+  }
+
+  githubElement.textContent = text;
+
+  if (!isDeleting && text === githubText) {
+    isDeleting = true;
+    setTimeout(typeDescription, 1000);
+  } else if (isDeleting && text === 'G') {
+    isDeleting = false;
+    setTimeout(() => {
+      index = 0;
+      githubElement.textContent = '';
+      typeEffect();
+    }, 1000);
   }
 
   let typingSpeed = isDeleting ? 50 : 200;
-
   timer = setTimeout(typeEffect, typingSpeed);
 }
 
 function typeDescription() {
+  let text = descriptionText.substring(0, index);
+  descriptionElement.textContent = text;
+
   if (index < descriptionText.length) {
-    descriptionElement.textContent += descriptionText[index];
     index++;
     timer = setTimeout(typeDescription, 100);
   } else {
     clearTimeout(timer);
     index = 0;
-    descriptionElement.textContent = '';
-    setTimeout(typeEffect, 1000);
+    setTimeout(() => {
+      descriptionElement.textContent = '';
+      typeEffect();
+    }, 1000);
   }
 }
 
-setTimeout(typeEffect, 1000);
-setTimeout(typeDescription, 2000);
+function startAnimation() {
+  setTimeout(typeEffect, 1000);
+}
+
+startAnimation(); // Start the animation
